7b32dd2661f08a5e0cc13731164a8f34b062bc7f
Richard McSharry <Richard McSharry>
2016-09-02 00:27:23 +0200

    Initializes a few settings for json api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Adds logging to the terminal, greatly aids debugging and seeing which routes and DB queries are being used
+  Rails.logger = Logger.new(STDOUT)
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
