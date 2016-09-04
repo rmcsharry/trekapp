03fd64c256e5eb1196d6c8998de43208c06b6b5c
Richard McSharry <Richard McSharry>
2016-09-04 17:40:27 +0200

    Adds model and serializer for people api route

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+    # in reverse order of importance - ie higher integer in the db = higher importance
+    enum status: [:unknown, :no_longer_employed, :standby, :active]
 end
--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,3 +1,11 @@
 class PersonSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name
+  attributes :id, :first_name, :last_name, :status, :status_number
+
+  def status
+    return object.status.dasherize
+  end
+
+  def status_number
+    return Person.statuses[object.status]
+  end
 end
