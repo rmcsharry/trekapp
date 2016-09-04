a5965eb481bdb6110320523d469b87adefcc4c55
Richard McSharry <Richard McSharry>
2016-09-04 17:42:19 +0200

    Adds status to person model

--- trekclient/app/models/person.js
+++ trekclient/app/models/person.js
@@ -1,8 +1,14 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr(),
   lastName: DS.attr(),
+  status: DS.attr(),
+  statusNumber: DS.attr(),
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('lastName')}, ${this.get('firstName')}`;
+  }),
 
   treks: DS.hasMany('trek'),
 });
