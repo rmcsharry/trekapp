e7570134d05328be86d009f6a581effe90e4d85f
Richard McSharry <Richard McSharry>
2016-09-08 14:57:04 +0200

    Renames loop enum to looped

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -3,7 +3,7 @@ class Trail < ApplicationRecord
 
     default_scope { order("updated_at DESC") }
 
-    enum distance_type: [:undefined, :loop, :out_and_back, :point_to_point]
+    enum distance_type: [:undefined, :looped, :out_and_back, :point_to_point]
     enum publish_status: [:draft, :published]
 
 end
