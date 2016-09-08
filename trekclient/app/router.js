b461bf37ec6d8829ed7948b056ff398afa854771
Richard McSharry <Richard McSharry>
2016-09-08 16:32:07 +0200

    Makes code layout consistent

--- trekclient/app/router.js
+++ trekclient/app/router.js
@@ -6,14 +6,14 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('home');
 
-  this.route('trails', function() {
+  this.route('trails', function () {
     this.route('new');
   });
 
-  this.route('employees', function() {
+  this.route('employees', function () {
     this.route('new');
   });
 
