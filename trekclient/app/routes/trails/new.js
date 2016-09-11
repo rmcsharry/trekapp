9c7c2a55ca3029d1ad4e4b3de6ba8e8e1a330372
Richard McSharry <Richard McSharry>
2016-09-11 11:51:36 +0200

    Adds a simple D&D component

--- trekclient/app/routes/trails/new.js
+++ trekclient/app/routes/trails/new.js
@@ -4,9 +4,14 @@ export default Ember.Route.extend({
   isEditing: true,
 
   // need to do this until routeable components are available
-  setupController(controller) {
-      this._super(...arguments);
-      controller.set('trailsNewRoute', this);
-  },
+  // setupController(controller) {
+  //     this._super(...arguments);
+  //     controller.set('trailsNewRoute', this);
+  // },
 
+  actions: {
+    fileDropped: function(obj, ops) {
+      console.log("dropped");
+    }
+  }
 });
