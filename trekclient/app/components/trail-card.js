2badf2e40e1fb3fa77c96cfc0715bdae04eaa82b
Richard McSharry <Richard McSharry>
2016-09-11 12:05:04 +0200

    Adjusts scroll into view bottom margin

--- trekclient/app/components/trail-card.js
+++ trekclient/app/components/trail-card.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 import InViewportMixin from 'ember-in-viewport';
 
 export default Ember.Component.extend(InViewportMixin, {
-  classNameBindings: [ 'viewportEntered:highlight' ]
+  classNameBindings: [ 'viewportEntered:highlight' ],
+  viewportOptionsOverride: Ember.on('didInsertElement', function() {
+    Ember.setProperties(this, {
+      viewportTolerance: {
+        top    : 0,
+        bottom : 40, // causes viewportEntered to fire when 40px is still left to scroll into view
+        left   : 0,
+        right  : 0
+      }
+    });
+  })  
 });
