bdd710f74f125dbe9070cc69adc16a63f5cb481e
Richard McSharry <Richard McSharry>
2016-09-12 23:19:55 +0200

    Loads filepicker when needed

--- trekclient/app/routes/trails/new.js
+++ trekclient/app/routes/trails/new.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   isEditing: true,
 
+  beforeModel: function() {
+      return Ember.$.getScript('//api.filestackapi.com/filestack.js');
+  }
+
   // need to do this until routeable components are available
   // setupController(controller) {
   //     this._super(...arguments);
