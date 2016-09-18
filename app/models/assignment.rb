411e0347d4bd30024486682f75710252c9bce4cc
Richard McSharry <Richard McSharry>
2016-09-18 22:05:17 +0200

    Removes scopes, not needed

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,8 +3,8 @@ class Assignment < ApplicationRecord
   belongs_to :trail
 
   # NOTE: if addding _ items, they need to be dasherized in the serializer
-  enum status: [:unknown, :pending, :active]  
+  enum status: [:unknown, :pending, :active]
 
-  scope :active, -> { where(status: [Assignment.statuses[:active]]) }
-  scope :pending, -> { where(status: [Assignment.statuses[:pending]]) }
+#  scope :active, -> { where(status: [Assignment.statuses[:active]]) }
+#  scope :pending, -> { where(status: [Assignment.statuses[:pending]]) }
 end
