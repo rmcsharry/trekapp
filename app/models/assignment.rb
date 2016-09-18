c98371f9365ff253e32a47bf5af7e20cfbfca14d
Richard McSharry <Richard McSharry>
2016-09-18 22:16:53 +0200

    Adds missing word RECORDING

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,10 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :person
   belongs_to :trail
-
+  
   # NOTE: if addding _ items, they need to be dasherized in the serializer
   enum status: [:unknown, :pending, :active]
-
-#  scope :active, -> { where(status: [Assignment.statuses[:active]]) }
-#  scope :pending, -> { where(status: [Assignment.statuses[:pending]]) }
 end
