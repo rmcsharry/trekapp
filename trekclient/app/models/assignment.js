92b5fdcd8ac3b733d322c9f3143a7cef84cc337f
Richard McSharry <Richard McSharry>
2016-09-21 22:20:52 +0200

    Adds status to model, fixes template

--- trekclient/app/models/assignment.js
+++ trekclient/app/models/assignment.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  status: DS.attr(),
+
   // relationships
   trail: DS.belongsTo('trail', { async: true }),
   person: DS.belongsTo('person', { async: true }),
