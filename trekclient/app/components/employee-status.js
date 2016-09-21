32fddb1e5c2b59f7a74a0c0a12d5cc7cfb4b615d
Richard McSharry <Richard McSharry>
2016-09-21 22:19:02 +0200

    Fixes status to use _ instead of -

--- trekclient/app/components/employee-status.js
+++ trekclient/app/components/employee-status.js
@@ -3,12 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   statusText: null,
 
+  // TODO Refactor this into a template helper so we can use it for other enums
   statusHumanized: Ember.computed( function() {
     // Converts underscores to spaces and capitalizes each word
-    // faster than regex, see: http://goo.gl/oyfxGM 
-    let frags = this.get('statusText').toString().split('-');
+    // This is faster than regex, see: http://goo.gl/oyfxGM 
+    let frags = this.get('statusText').toString().split('_');
     for (let i=0; i<frags.length; i++) {
-        frags[i] = frags[i].capitalize(); // frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+        frags[i] = frags[i].capitalize(); // eq to this: frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
     }
     return frags.join(' ');
   }),
